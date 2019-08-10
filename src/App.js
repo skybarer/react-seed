76fc4597d30ab68aa970f16fe71ad39cd0062efd
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-08-10 14:00:06 +0530

    added favicon

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo flipInY" alt="logo" />
-        <p>
-         alpha
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h4>alpha</h4>
+			</header>
+		</div>
+	);
 }
 
 export default App;
